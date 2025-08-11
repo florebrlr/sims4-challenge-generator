5e081bf29b1b881638896529b6b1d9cb463a8d07
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-08-11 21:07:36 +0200

    🔧  ajout des autres catégories dans categories.json

--- sims4-challenge-generator/src/views/Home.vue
+++ sims4-challenge-generator/src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
-<main>
-<div> KAKOU KAKOU !</div>
-</main>
+  <main>
+    <h1>Structure de la page</h1>
+    <div>
+      <p>menu</p>
+      <p>ChallengeDetail</p>
+      <p>Générer un random challenge</p>
+    </div>
+  </main>
 </template>
 
-<script setup lang="js">
+<script setup lang="js"></script>
 
-</script>
-
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
