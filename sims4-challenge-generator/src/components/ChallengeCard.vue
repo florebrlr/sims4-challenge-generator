774846162dd9b5af473795679e5cc235e1a9d0a2
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-08-11 21:37:28 +0200

    🚧  compoenent ChallengeCard.vue

---
+++ sims4-challenge-generator/src/components/ChallengeCard.vue
@@ -0,0 +1,42 @@
+<template>
+<h2>Random Challenge</h2>
+<div>
+  <ul>
+    <li>
+      <span>Style de la construction :</span>
+      <!-- <span>{{ challenge.style }}</span> -->
+    </li>
+    <li>
+      <span>Avec le(s) pack(s) :</span>
+    </li>
+        <li>
+      <span>Avec les couleurs :</span>
+    </li>
+        <li>
+      <span>Nombre de pièces :</span>
+    </li>
+        <li>
+      <span>Foyer de :</span>
+    </li>
+        <li>
+      <span>Budget :</span>
+    </li>
+        <li>
+      <span>Sur un lot de :</span>
+    </li>
+        <li>
+      <span>Restriction(s) supplémentaire(s) :</span>
+    </li>
+  </ul>
+</div>
+<div>
+  <button>Enregistrer</button>
+  <button>Nouveau Challenge</button>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
