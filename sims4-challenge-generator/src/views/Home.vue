db062e7e6747f1bbdc201b57809cf3f772b56bd4
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-08-22 14:45:02 +0200

    🚧 home

--- sims4-challenge-generator/src/views/Home.vue
+++ sims4-challenge-generator/src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <main>
     <h1>Sul Sul !Bienvenue dans le générateur de challenges Sims 4</h1>
+    <!--DIV random challenge-->
+    <div class="challenge-container">
+      <h2>Random Challenge</h2>
+      <p>desciption du Random Challenge ici</p>
+      <img src="" alt="" />
+      <button>Créer mon Random Challenge</button>
+    </div>
+    <!--DIV wheel challenge-->
+    <div class="challenge-container">
+      <h2>Random Challenge</h2>
+      <p>desciption du Random Challenge ici</p>
+      <img src="" alt="" />
+      <button>Créer mon Random Challenge</button>
+    </div>
   </main>
 </template>
 
