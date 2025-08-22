10428000795866bbf8d22611209fdac4caaf51d1
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-08-22 14:52:06 +0200

    🍱 add footer

--- sims4-challenge-generator/src/App.vue
+++ sims4-challenge-generator/src/App.vue
@@ -3,14 +3,16 @@
   <div id="app">
     <NavBar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
+import NavBar from '@/components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
-  name: "App",
-  components: { NavBar }
-};
+  name: 'App',
+  components: { NavBar, Footer },
+}
 </script>
---
+++ sims4-challenge-generator/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>© Tea-Rex - 2025</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 1rem;
+}
+</style>
